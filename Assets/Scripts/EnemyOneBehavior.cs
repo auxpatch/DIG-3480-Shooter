b06f68bbce7b088d5977ec650c07001b801163a8
Jacob <panaccionej@gmail.com>
2023-11-05 21:30:37 -0500

    ENEMIES
    Added Enemy Prefabs and Scripts.

---
+++ Assets/Scripts/EnemyOneBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOneBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3);
+        if (transform.position.y < -8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject enemyOnePrefab;
+    public GameObject enemyTwoPrefab;
+    public GameObject enemyThreePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
+        InvokeRepeating("CreateEnemyTwo", 2.0f, 4.0f);
+        InvokeRepeating("CreateEnemyThree", 0.5f, 5.0f);
     }
 
     // Update is called once per frame
@@ -20,6 +24,16 @@ public class GameManager : MonoBehaviour
 
     void CreateEnemyOne()
     {
-        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-6, 9), 7, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyTwo()
+    {
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-8, 4), 7, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyThree()
+    {
+        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-7, 7), 7, 0), Quaternion.identity);
     }
 }
